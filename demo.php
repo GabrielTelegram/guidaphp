905fb8de700b13817faf346f38940e1e6297cc37
GabrielTelegram <gabrieldeveloperita@gmail.com>
2016-07-22 22:04:04 +0200

    Semplice introduzione
    Una demo molto semplice per introdurre al .php con Telegram

--- demo.php
+++ demo.php
@@ -10,7 +10,7 @@
 require 'class-http-request.php';
 
 $api = "bot";
-$api .="111111111akskfiwkcjakckaoekfiakkwkzk";
+$api .="TUOTOKENRICAVABILEDA @botfather";
 global $api;
 
 $content = $_POST['input'];
